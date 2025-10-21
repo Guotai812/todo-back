b869ebe7fdd2bfaa3b541e9b64d4ee04c5743be4
guotai <guotaixiao87@gmail.com>
2025-10-22 10:09:50 +1100

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // This represents the Tasks table in your database
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApp.Models;
+
+public class TaskItem
+{
+    public string id { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    
+}
