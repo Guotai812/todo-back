8f1a4c6bd27580da59d7cf84e3d8f5897b6266f8
Guotai812 <guotaixiao87@gmail.com>
2025-11-05 21:46:18 +1100

    fix migration

---
+++ Migrations/20251103213727_AddCategoryToTodo.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToTodo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "categoryId",
+                table: "TaskItems",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    color = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskItems_categoryId",
+                table: "TaskItems",
+                column: "categoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskItems_Categories_categoryId",
+                table: "TaskItems",
+                column: "categoryId",
+                principalTable: "Categories",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskItems_Categories_categoryId",
+                table: "TaskItems");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskItems_categoryId",
+                table: "TaskItems");
+
+            migrationBuilder.DropColumn(
+                name: "categoryId",
+                table: "TaskItems");
+        }
+    }
+}
